d23af95b29c29293bb645062f0cef2317d4507fd
Zsombor <szilagyizs@kkszki.hu>
2025-12-12 09:13:33 +0100

    Bekérés és kapcsolat létrehozása

--- iktmikulas/Program.cs
+++ iktmikulas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace iktmikulas
 {
@@ -10,6 +11,16 @@ namespace iktmikulas
     {
         static void Main(string[] args)
         {
+            //Establishing connection to MySQL database
+            MySqlConnection connector = new MySqlConnection();
+            connector.ConnectionString = "server=localhost;user=root;password=;database=mikulas";
+            connector.Open();
+            Console.WriteLine("Sikeres csatlakozás a MySQL adatbázishoz.");
+            //File reading
+            MySqlCommand command = new MySqlCommand("SELECT * FROM versenyzok ORDER BY Pont1 DESC, Ido1 ASC;", connector);
+            MySqlDataReader reader = command.ExecuteReader();
+            connector.Close();
         }
     }
 }
+
